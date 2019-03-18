22639d7dee1b220a76ed2a09aa31ba639c05426e
Tobias Lückel <lueckel@solutiondrive.de>
2019-03-18 16:30:48 +0100

    Adds missing versions to build-args

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -4,7 +4,9 @@ set -e
 
 docker build \
     --build-arg PACKER_VERSION=$PACKER_VERSION  \
-    --build-arg ANSIBLE_VERSION=$ANSIBLE_VERSION  \
+    --build-arg AWSCLI_VERSION=$AWSCLI_VERSION \
+    --build-arg INSPEC_VERSION=$INSPEC_VERSION \
+    --build-arg ANSIBLE_VERSION=$ANSIBLE_VERSION \
     -t solutiondrive/docker-aws-packer-ansible:packer$PACKER_VERSION-ansible$ANSIBLE_VERSION-awscli$AWSCLI_VERSION-inspec$INSPEC_VERSION  \
     .
 

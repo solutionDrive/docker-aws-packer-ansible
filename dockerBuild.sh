09b4445e4958fd5f6c88fdf54d5d7537dc929cb5
Tobias Lückel <lueckel@solutiondrive.de>
2019-09-25 10:54:41 +0200

    Adds a TEST prefix

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -7,7 +7,7 @@ docker build \
     --build-arg AWSCLI_VERSION=$AWSCLI_VERSION \
     --build-arg INSPEC_VERSION=$INSPEC_VERSION \
     --build-arg ANSIBLE_VERSION=$ANSIBLE_VERSION \
-    -t solutiondrive/docker-aws-packer-ansible:packer$PACKER_VERSION-ansible$ANSIBLE_VERSION-awscli$AWSCLI_VERSION-inspec$INSPEC_VERSION  \
+    -t solutiondrive/docker-aws-packer-ansible:TEST-packer$PACKER_VERSION-ansible$ANSIBLE_VERSION-awscli$AWSCLI_VERSION-inspec$INSPEC_VERSION  \
     .
 
 # Tag "latest"

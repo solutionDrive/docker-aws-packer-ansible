9599a4aaa87600353c853a86132570555a7ad41a
Tobias Lückel <tobias.lueckel@netlogix.de>
2020-07-15 15:32:41 +0200

    Explicit set the envrionment variables for the packer run because otherwise they are not available

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 
 /bin/addSSHKey.sh
 . /bin/aquireAWSSession.sh
-/bin/packerBuild.sh
+AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN /bin/packerBuild.sh
